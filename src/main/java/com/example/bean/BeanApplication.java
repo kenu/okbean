513e433c289210526ac64ebde232d205f5f10e58
kenu <kenu.heo@gmail.com>
2021-02-01 18:32:43 +0900

    bean init

---
+++ src/main/java/com/example/bean/BeanApplication.java
@@ -0,0 +1,23 @@
+package com.example.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanApplication implements CommandLineRunner {
+
+	@Autowired
+	MemberKenu kenu;
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		kenu.displayInfo();
+		// add line
+	}
+}
---
+++ src/main/java/com/example/bean/MemberKenu.java
@@ -0,0 +1,10 @@
+package com.example.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberKenu {
+    public void displayInfo() {
+        System.out.println("Kenu");
+    }
+}
