9d17ed2a4861c84fd1022dd52a9e471ce3fec86d
d36choi <yuio1126@gmail.com>
2021-02-01 23:04:13 +0900

    feat: Add MemberD36choi. (choi sanghyun)

--- src/main/java/com/example/bean/BeanApplication.java
+++ src/main/java/com/example/bean/BeanApplication.java
@@ -11,6 +11,9 @@ public class BeanApplication implements CommandLineRunner {
 	@Autowired
 	MemberKenu kenu;
 
+	@Autowired
+	MemberD36choi memberD36choi;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BeanApplication.class, args);
 	}
@@ -19,5 +22,6 @@ public class BeanApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		kenu.displayInfo();
 		// add line
+		memberD36choi.displayInfo();
 	}
 }
