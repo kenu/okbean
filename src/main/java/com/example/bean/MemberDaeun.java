cdec0482f767fb6ce1a5b303419c0ec1b953a04a
jeongdaeun98 <uneap@sk.com>
2021-02-02 09:38:21 +0900

    feat: create MemberDaeun

---
+++ src/main/java/com/example/bean/MemberDaeun.java
@@ -0,0 +1,11 @@
+package com.example.bean;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberDaeun {
+    public void displayInfo() {
+        System.out.println("jeongdaeun");
+    }
+}
